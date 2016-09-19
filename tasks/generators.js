01ed8d64fa560a4d141e66559add4d2e71632d2e
Glenn Wedin <glenn_wedin@hotmail.com>
2016-09-20 01:04:10 +0200

    generator

--- tasks/generators.js
+++ tasks/generators.js
@@ -4,9 +4,19 @@ var list = ["Leonora", "Max Mekker", "Alfa", "Bernt"];
 *	Write a generator function to iterate through this list.
 *	The tests expects to be able to use "next()" to receive the next item from the list;
 */
-
-
-export default generator;
+function fakeGenerator () {
+	var l = list.length, obj;
+	return {
+		next: function () {
+		 	obj = {
+				value: list[--l],
+				done: (l < 0)
+			}
+			return obj;
+		}
+	}
+}
+export default fakeGenerator;
 
 
 
